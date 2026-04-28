bc750de1b9de3f46eca6e82da60f577d89e92062
MariaClaraGomesCardoso <mcgc3@aluno.ifnmg.edu.br>
2026-04-28 18:40:00 +0000

    feat : JavaScript: ES Modules no Node - aula 08

--- exercicios/aula08/utils/matematica.js
+++ exercicios/aula08/utils/matematica.js
@@ -5,17 +5,24 @@
 // Todas recebem dois números (a, b) e retornam o resultado da operação.
 
 export function somar(a, b) {
+  return a + b
   // escreva seu código aqui
 }
 
 export function subtrair(a, b) {
+  return a - b
   // escreva seu código aqui
 }
 
 export function multiplicar(a, b) {
+  return a * b
   // escreva seu código aqui
 }
 
 export function dividir(a, b) {
+  if (b === 0){
+    return null
+  }
+  return a/b
   // escreva seu código aqui
 }
--- exercicios/aula08/utils/texto.js
+++ exercicios/aula08/utils/texto.js
@@ -3,5 +3,9 @@
 // ========================================
 // Implemente a função e exporte com "export default".
 export default function formatarNome(nome) {
+   const minusculo = nome.toLowerCase()
+   const primeiraLetra = minusculo.charAt(0).toUpperCase()
+   const restante = minusculo.slice(1)
+   return primeiraLetra + restante
   // escreva seu código aqui
 }
